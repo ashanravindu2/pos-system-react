4699e089239259b1176c5c36c03eea521d39456f
Ashan Ravindu Lakshitha <139869812+ashanravindu2@users.noreply.github.com>
2025-01-05 01:29:21 +0530

    react folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import {Provider} from "react-redux";
+import {store} from "./store/Store.ts";
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>,
 )
