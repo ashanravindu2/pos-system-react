e79cd238b8e8ac70a2412ce7d538596eaaf2b7cf
Ashan Ravindu Lakshitha <139869812+ashanravindu2@users.noreply.github.com>
2025-01-14 04:00:50 +0530

    feat: add 404 Error page with animation

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -3,11 +3,22 @@ import {Outlet} from "react-router-dom";
 
 
 
+
 export function RootLayout() {
     return (
         <>
-            <Navigation/>
-            <Outlet/>
+            <div className={' grid-cols-2 flex'}>
+
+                    <Navigation/>
+
+                <div className={'ml-56  flex'}>
+                        <Outlet/>
+                </div>
+
+
+            </div>
+
+
         </>
     );
 }
